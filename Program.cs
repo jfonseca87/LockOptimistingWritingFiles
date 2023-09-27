7dce0b2e3bb51a61f894e2045182658a3e86d562
Jorge Fonseca <jorge.fonseca87@gmail.com>
2023-09-27 17:55:25 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+namespace LockOptimistingWritingFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Delete("file.txt");
+
+            int interactions = 5;
+            Thread[] threads = new Thread[interactions];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                var t = new Thread(() =>
+                {
+                    FileWriter fileWriter = new FileWriter();
+                    fileWriter.WriteVersionOne();
+                    fileWriter.WriteVersionTwo();
+                });
+                t.Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    internal class FileWriter
+    {
+        /* 
+         * I create this object as static because when WriteVersionTwo is called in the same thread
+         * this object already exists and doesn't allow to start writing unless WriteVersionOne finish
+        */
+        private static object locker = new object();
+
+        public void WriteVersionOne()
+        {
+            Console.WriteLine("One");
+            lock (locker)
+            {
+                StreamWriter sw = new StreamWriter("file.txt", true);
+                for (int i = 0; i < 5; i++)
+                {
+                    sw.WriteLine($"One - {i + 1}");
+                }
+                sw.Close();
+            }
+        }
+
+        public void WriteVersionTwo()
+        {
+            Console.WriteLine("Two");
+            lock (locker)
+            {
+                StreamWriter sw = new StreamWriter("file.txt", true);
+                for (int i = 0; i < 5; i++)
+                {
+                    sw.WriteLine($"Two - {i + 1}");
+                }
+                sw.Close();
+            }
+        }
+    }
+}
